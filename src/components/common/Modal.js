6f77de0086722445285b64582c214e005ca0ee17
An Nguyen <nguyenan8196@gmail.com>
2021-04-26 13:44:49 +0700

    feat: refactor fe

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,16 +1,33 @@
 import React from "react";
 import {showModal} from "../../app/features/modalSlice";
 import {useDispatch, useSelector} from 'react-redux';
-import Dialog from "@material-ui/core/Dialog";
+import CloseIcon from "@material-ui/icons/Close";
+import {Dialog, DialogContentText, DialogTitle, DialogContent, DialogActions} from "@material-ui/core";
 
-const Modal = ({ children }) => {
+const Modal = ({title, description, content, action}) => {
     const dispatch = useDispatch();
-    const count = useSelector((state) => state.modal.value);
+    const isShow = useSelector((state) => state.modal.value);
 
     return (
         <>
-            <Dialog open={count} onClose={() => dispatch(showModal())} aria-labelledby="form-dialog-title" fullWidth={true} maxWidth={'xs'}>
-                {children}
+            <Dialog open={isShow} onClose={() => dispatch(showModal())} aria-labelledby="form-dialog-title"
+                    fullWidth={true} maxWidth={'xs'}>
+                <DialogTitle id="form-dialog-title">
+                    <span>{title}</span>
+                    <CloseIcon style={{fontSize: 22}} className="cursor-pointer float-right"
+                               onClick={() => dispatch(showModal())}/>
+                </DialogTitle>
+                <DialogContent className="flex flex-col space-y-2 w-2/3 text-center my-0 mx-auto">
+                    <DialogContentText>
+                        {description}
+                    </DialogContentText>
+                    {content}
+                </DialogContent>
+                {action &&
+                <DialogActions>
+                    {action}
+                </DialogActions>
+                }
             </Dialog>
         </>
     )
--- src/components/layouts/Login.js
+++ src/components/layouts/Login.js
@@ -3,56 +3,34 @@ import "../../assets/styles/Login.css";
 import {useDispatch} from 'react-redux';
 import {showModal} from "../../app/features/modalSlice";
 import Modal from "../common/Modal";
-import PersonIcon from "@material-ui/icons/Person";
 import IconButton from "@material-ui/core/IconButton";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import {Person, Facebook, Twitter} from '@material-ui/icons';
+
+const Content = () => <>
+    <Button
+        variant="outlined"
+        startIcon={<Facebook className="facebook"/>}
+    >
+        Facebook
+    </Button>
+    <Button
+        variant="outlined"
+        startIcon={<Twitter className="twitter"/>}
+    >
+        Twitter
+    </Button>
+</>;
 
 export default function Login() {
     const dispatch = useDispatch();
 
-    function login() {
-        alert('Login successfully')
-    }
-
     return (
         <>
-            <IconButton className="h-full focus:outline-none" onClick={() => dispatch(showModal())}>
-                <PersonIcon className="text-white"/>
+            <IconButton className="h-full" onClick={() => dispatch(showModal())}>
+                <Person className="text-white"/>
             </IconButton>
-            <Modal>
-                <DialogTitle id="form-dialog-title">Login</DialogTitle>
-                <DialogContent className="flex flex-col space-y-2 w-2/3 text-center my-0 mx-auto">
-                    <DialogContentText>
-                        Welcome back.
-                    </DialogContentText>
-                    <Button
-                        variant="outlined"
-                        startIcon={<FacebookIcon className="facebook"/>}
-                    >
-                        Facebook
-                    </Button>
-                    <Button
-                        variant="outlined"
-                        startIcon={<TwitterIcon className="twitter"/>}
-                    >
-                        Twitter
-                    </Button>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={login} variant="contained">
-                        CREATE
-                    </Button>
-                    <Button onClick={() => dispatch(showModal())} variant="contained">
-                        CLOSE
-                    </Button>
-                </DialogActions>
-            </Modal>
+            <Modal title="Login" description="Welcome back!" content={<Content/>}/>
         </>
     );
 }
